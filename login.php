033c50000c7e91dd105d8a08a8b1498f2804a93f
Brahim Aimen <bramen.mail@gmail.com>
2021-03-23 15:27:22 +0100

    Frontend Login and Signup ready

---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Realtile Chat App</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="wrapper">
+		<section class="form login">
+			<header>Realtime Chat App</header>
+			<form action="">
+				<div class="error-txt">This is an error message!</div>
+					<div class="field input">
+						<label>Email</label>
+						<input type="text" name="" placeholder="Enter your email">
+					</div>
+					<div class="field input">
+						<label>Password</label>
+						<input type="text" name="" placeholder="Enter your password">
+						<i class="fas fa-eye eye"></i>
+					</div>
+					<div class="field button">
+						<input type="submit" value="Continue to Chat">
+					</div>
+
+			</form>
+			<div class="link">Haven't an account ? <a href="#">Sign up</a></div>
+		</section>
+		
+	</div>
+
+</body>
+</html>
